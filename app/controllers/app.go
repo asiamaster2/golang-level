4da46a2a26dd9b1f51d4ee99ae0e5cbe9c15b296
root <root@ubuntu-xenial-1.c.aaron-project-197520.internal>
2018-03-09 21:15:11 +0000

    4th commit

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -11,3 +11,12 @@ type App struct {
 func (c App) Index() revel.Result {
 	return c.Render()
 }
+
+func (c App) Create(username, password string) revel.Result {
+    if username == "hylee" && password == "aaron11!" {
+        var resultmsg string = "1.1.1.1"
+        return c.Render(resultmsg)
+    }
+    var resultmsg string = "Please check your credential."
+    return c.Render(resultmsg)
+}
